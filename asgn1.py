460a3ace63a375279d39e2a17429a0f37ffab1c4
pinak <pinakm9@gmail.com>
2017-09-13 20:27:18 +0530

    final asgn1 1

--- asgn1.py
+++ asgn1.py
@@ -18,13 +18,13 @@ def assignment_1_i():
 
 	p = op.minimize_scalar(loss, bounds=(1e-10, 1e-2), method='bounded').x
 	result = lambda t: model(t, p)
-	print('Best fit p = {}, relative error = {}%\n{}'.format(p, rel_err(ecdf, result, [0, 73]), div))
+	print('Best fit p = {:.2e}, relative error = {:.2}%\n{}'.format(p, rel_err(ecdf, result, [0, 73]), div))
 	fit_plot(ecdf, result, [0, 73], 'age in months at the time of diagnosis',\
 	 'probability of being diagnosed with retinoblastoma', 'single random factor (unilateral)')
 
 def assignment_1_ii():
 	global p1, p2
-	print('Problem 1(ii)\n{}'.format(div))
+	print('Problem 1(ii)(requires ~3 seconds to finish calculation)\n{}'.format(div))
 	
 	def model(t, p):
 		i, T =  1, int(t)
@@ -40,7 +40,8 @@ def assignment_1_ii():
 
 	p1, p2 = op.minimize(loss, [40e-8, 1e-8]).x
 	result = lambda t: model(t, [p1, p2])
-	print('p1 = {}, p2 = {}, relative error =  {}%\n{}'.format(p1, p2, rel_err(ecdf, result, [0, 36]), div))
+	print('p1 = {:.2e}, p2 = {:.2e},\nrelative error upto 36 months =  {:.2}%, overall relative error = {:.2}%\n{}'\
+		.format(p1, p2, rel_err(ecdf, result, [0, 36]), rel_err(ecdf, result, [0, 73]), div))
 	fit_plot(ecdf, result, [0, 73], 'age in months at the time of diagnosis',\
 	 'probability of being diagnosed with retinoblastoma', 'two random factors (unilateral)')
 
@@ -67,35 +68,37 @@ def assignment_1_iii():
 
 	a = op.minimize_scalar(loss, bounds=(0, 5), method='bounded').x
 	result = lambda t: model(t, a)
-	print('a = {}, relative error =  {}%\n{}'.format(a, rel_err(ecdf, result, [0, 73]), div))
+	print('a = {:.3}, relative error =  {:.2}%\n{}'.format(a, rel_err(ecdf, result, [0, 73]), div))
 	fit_plot(ecdf, result, [0, 73], 'age in months at the time of diagnosis',\
 	 'probability of being diagnosed with retinoblastoma', 'decaying p2 (unilateral)')
 				
 
 def assignment_1_iv():
-	print('Problem 1(iv)\n{}'.format(div))
+	print('Problem 1(iv) (requires ~8 seconds to finish calculation)\n{}'.format(div))
 
 	def model(t, a):
 		i, T =  1, int(t)
-		total = (1-p1)**T
+		_p1 = lambda j: p1 if j <= 36 else p1*np.exp(-a*(j-36))
+		total = 1
+		while i <= T:
+			total = total*(1-_p1(i))
+			i = i+1
+		i=1
 		while i <= T:
 			j, prod = 1, 1
 			while j <= i-1:
-				if j <= 36:
-					prod = prod*(1-p1)
-				else:
-					prod = prod*(1-p1*np.exp(-a*(j-36)))
+				prod = prod*(1-_p1(j))
 				j = j+1
-			total = total + p1*(1-p2)**(T-i)*prod
+			total = total + _p1(i)*(1-p2)**(T-i)*prod
 			i = i+1
 		return (1 - total**(2*cell_count))
 
 	def loss(a):
 		return integrate.quad(lambda t: (ecdf(t)-model(t, a))**2, 0, 73)[0]
 
-	a = op.minimize_scalar(loss, bounds=(0, 10), method='bounded').x
+	a = op.minimize_scalar(loss).x
 	result = lambda t: model(t, a)
-	print('a = {}, relative error =  {}%\n{}'.format(a, rel_err(ecdf, result, [0, 73]), div))
+	print('a = {:.3}, relative error =  {:.2}%\n{}'.format(a, rel_err(ecdf, result, [0, 73]), div))
 	fit_plot(ecdf, result, [0, 73], 'age in months at the time of diagnosis',\
 	 'probability of being diagnosed with retinoblastoma', 'decaying p1 (unilateral)')
 				
